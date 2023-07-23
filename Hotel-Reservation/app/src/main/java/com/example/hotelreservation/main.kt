8b8c0487d1a2daeda363f810ffc92ffe21f7ac3c
ars-yeon <zosel7301@gmail.com>
2023-07-23 17:28:19 +0900

    [Feat]: ⚙️ - `try-catch`로 숫자 이외의 값 예외 처리 - Check 클래스 연동

--- Hotel-Reservation/app/src/main/java/com/example/hotelreservation/main.kt
+++ Hotel-Reservation/app/src/main/java/com/example/hotelreservation/main.kt
@@ -1,44 +1,55 @@
 package com.example.hotelreservation
 
 fun main() {
-    var rez = Reservation()
+    val rez = Reservation()
+    val check = Check()
 
     println("호텔예약 프로그램 입니다.")  // println("Hotel Reservation Program")
     println("=============================================================================")
-    println("[메뉴]\n1.방예약  2.예약 목록 출력  3.예약 목록 출력(정렬)  4.시스템 종료  5.입출금 내역 출력  6.예약 변경/취소")
+    println("[메뉴]\n1.방예약  2.예약 목록 출력  3.예약 목록 출력(정렬)  4.시스템 종료  5.입출금 내역 출력  6.예약 변경/취소 7. 프로그램 종료")
     print("입력: ")
 
     while (true) {
-        var menu = readLine()!!.toInt()
+        try {
+            var menu = readLine()!!.toInt()
 
-        when (menu) {
-            1 -> {
-                rez.reserve()
-                println("호텔 예약이 완료되었습니다.")
-            }
+            when (menu) {
+                1 -> {
+                    rez.reserve()
+                    println("호텔 예약이 완료되었습니다.")
+                }
 
-            2 -> {
-                println("예약 목록 출력")
-            }
+                2 -> {
+                    check.check(rez.reservations)
+                }
 
-            3 -> {
-                println("예약 목록 출력(정렬)")
-            }
+                3 -> {
+                    println("예약 목록 출력(정렬)")
+                }
 
-            4 -> {
-                println("시스템 종료")
-            }
+                4 -> {
+                    println("시스템 종료")
+                }
 
-            5 -> {
-                println("입출금 내역 출력")
-            }
+                5 -> {
+                    println("입출금 내역 출력")
+                }
 
-            6 -> {
-                println("예약 변경/취소")
+                6 -> {
+                    println("예약 변경/취소")
+                }
+                
+                7 -> {
+                    println("호텔 예약 프로그램을 종료합니다.")
+                    break
+                }
+
+                else -> {
+                    println("올바르지 않은 값을 입력하였습니다. 다시 입력해주세요.")  //println("Invalid number. Please try again.")
+                }
             }
-            else -> {
-                println("Invalid number. Please try again.")
-            }
+        } catch (e: NumberFormatException) {
+            println("올바른 숫자를 입력해주세요.")
         }
     }
 }
