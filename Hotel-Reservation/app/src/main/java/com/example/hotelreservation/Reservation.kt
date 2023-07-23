628e0940d387e5d8783727bae6d9d467dd0a1113
ars-yeon <zosel7301@gmail.com>
2023-07-23 13:45:30 +0900

    [Feat]: ⚙️ - 예약자 이름 입력 - 예약할 방 번호 입력 - 체크인 날짜 입력, 예외 처리 - 체크아웃 날짜 입력, 예외 처리

--- Hotel-Reservation/app/src/main/java/com/example/hotelreservation/Reservation.kt
+++ Hotel-Reservation/app/src/main/java/com/example/hotelreservation/Reservation.kt
@@ -1,23 +1,39 @@
 package com.example.hotelreservation
 
 class Reservation {
-    fun rez() {
+    var today: String = "20230723"
+    var room: String = ""
+    var checkIn: String = ""
+    var checkOut: String = ""
+
+    fun reserve() {
         println("예약자분의 성함을 입력해주세요.")
-        var rezName = readLine()!!
+        var name = readLine()!!
 
-        println("예약할 방 번호를 입력해주세요. (100~999)")
-        var rezRoom = readLine()!!.toInt()
+        while (true) {
+            println("예약할 방 번호를 입력해주세요. (100~999)")
+            room = readLine()!!
 
-        // 예약할 방 번호가 100~999에 속하지 않으면 예외처리
+            // 예약할 방 번호가 100~999에 속하지 않으면 예외 처리
+            if (room.toInt() in 100..999) break
+        }
 
-        println("체크인 날짜를 입력해주세요. (YYYYMMDD)")
-        var dateIn = readLine()!!.toInt()
-        
-        // 선택한 날짜가 지났으면 예외처리
+        while (true) {
+            println("체크인 날짜를 입력해주세요. (YYYYMMDD)")
+            checkIn = readLine()!!
 
-        println("체크아웃 날짜를 입력해주세요. (YYYYMMDD)")
-        var dateOut = readLine()!!.toInt()
+            // 선택한 날짜가 지났으면 예외 처리
+            if (checkIn.toInt() < today.toInt()) println("지난 날짜는 예약이 불가능합니다.")
+            else break
+        }
 
-        // 체크아웃 날짜가 체크인 날짜보다 전이거나 같으면 예외처리
+        while (true) {
+            println("체크아웃 날짜를 입력해주세요. (YYYYMMDD)")
+            checkOut = readLine()!!
+
+            // 선택한 날짜가 체크인 날짜와 같거나 전이라면 예외 처리
+            if (checkOut.toInt() <= checkIn.toInt()) println("체크인 날 당일 또는 지난 날짜는 예약이 불가능합니다.")
+            else break
+        }
     }
 }
