88b68b74e1642dc172bb119925b6ed2902ccaeee
ars-yeon <zosel7301@gmail.com>
2023-07-23 18:16:56 +0900

    [FIX] : 🔧 - 메뉴 목록, 입력을 while문 안으로 이동 - 정렬된 예약 목록 출력을 위한 val sort = CheckSort와 sort.checkSort(rez.reservations) 추가

--- Hotel-Reservation/app/src/main/java/com/example/hotelreservation/main.kt
+++ Hotel-Reservation/app/src/main/java/com/example/hotelreservation/main.kt
@@ -3,14 +3,16 @@ package com.example.hotelreservation
 fun main() {
     val rez = Reservation()
     val check = Check()
+    val sort = CheckSort()
 
     println("호텔예약 프로그램 입니다.")  // println("Hotel Reservation Program")
-    println("=============================================================================")
-    println("[메뉴]\n1.방예약  2.예약 목록 출력  3.예약 목록 출력(정렬)  4.시스템 종료  5.입출금 내역 출력  6.예약 변경/취소 7. 프로그램 종료")
-    print("입력: ")
+
 
     while (true) {
         try {
+            println("=================================================================================================")
+            println("[메뉴]\n1.방예약  2.예약 목록 출력  3.예약 목록 출력(정렬)  4.시스템 종료  5.입출금 내역 출력  6.예약 변경/취소 7. 프로그램 종료")
+            print("입력: ")
             var menu = readLine()!!.toInt()
 
             when (menu) {
@@ -24,7 +26,7 @@ fun main() {
                 }
 
                 3 -> {
-                    println("예약 목록 출력(정렬)")
+                    sort.checkSort(rez.reservations)
                 }
 
                 4 -> {
