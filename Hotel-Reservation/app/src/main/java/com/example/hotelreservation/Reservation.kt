1be48a24a4b4cce77d42e81ec94a0eab2c35a05b
ars-yeon <zosel7301@gmail.com>
2023-07-23 21:48:40 +0900

    [FEAT]: ⚙️ - 숙박비, 예약금 추가

--- Hotel-Reservation/app/src/main/java/com/example/hotelreservation/Reservation.kt
+++ Hotel-Reservation/app/src/main/java/com/example/hotelreservation/Reservation.kt
@@ -1,11 +1,18 @@
 package com.example.hotelreservation
 
+import java.util.Random
+
 class Reservation {
     var today: String = "20230723"
     var room: String = ""
     var checkIn: String = ""
     var checkOut: String = ""
     var reservations: MutableList<ReservationInfo> = mutableListOf()
+
+    val random = Random()
+    fun rand(from: Int, to: Int): Int {
+        return random.nextInt(to - from) + from
+    }
 
     fun reserve() {
         println("예약자분의 성함을 입력해주세요.")
@@ -37,8 +44,13 @@ class Reservation {
             else break
         }
 
+        val pay = rand(50000, 600000).toString()  // 호텔 숙소비 500000~600000 랜덤으로 임의의 금액
+        val deposit = (pay.toInt() * 0.1).toString()  // 예약금: 호텔 숙박비의 10%
+
         // 입력 받은 정보를 ReservationInfo 객체로 만들어서 리스트에 추가
-        val reservationInfo = ReservationInfo(name, room, checkIn, checkOut)
+        val reservationInfo = ReservationInfo(name, room, checkIn, checkOut, pay, deposit)
         reservations.add(reservationInfo)
+
+        println("호텔 숙박비 ${pay}원의 10%인 예약금 ${deposit}원을 입금해주세요!")
     }
 }
