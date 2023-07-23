4c3c364cfe7d63b26147c957a20e2faa9066f9e9
ars-yeon <zosel7301@gmail.com>
2023-07-23 17:49:24 +0900

    [Feat]: ⚙️ - 날짜를 YYYY-MM-DD 형식으로 변환하는 함수를 제거

--- Hotel-Reservation/app/src/main/java/com/example/hotelreservation/Check.kt
+++ Hotel-Reservation/app/src/main/java/com/example/hotelreservation/Check.kt
@@ -10,14 +10,11 @@ class Check {
 
         println("호텔 예약자 목록입니다.")
         for ((index, reservationInfo) in reservations.withIndex()) {
-            val formattedCheckIn = formatDate((reservationInfo.checkIn))
-            val formattedCheckOut = formatDate((reservationInfo.checkOut))
+            val formattedCheckIn = Date.formatDate((reservationInfo.checkIn))
+            val formattedCheckOut = Date.formatDate((reservationInfo.checkOut))
             val list = "${index + 1}. 예약자: ${reservationInfo.name}, 방 번호: ${reservationInfo.room}호, 체크인: ${formattedCheckIn}, 체크아웃: ${formattedCheckOut}"
             println(list)
         }
     }
-    // 날짜를 YYYY-MM-DD 형식으로 변환하는 함수
-    private fun formatDate(date: String): String {
-        return "${date.substring(0,4)}-${date.substring(4, 6)}-${date.substring(6, 8)}"
-    }
+
 }
