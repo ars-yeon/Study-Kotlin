aa7968c9dc48068683527dfc3e72c35f90837276
ars-yeon <zosel7301@gmail.com>
2023-08-02 17:04:48 +0900

    [RENAME] : ✏️ - MainActivity.kt → SignInActivity.kt - activity_main.xml → activity_signin.xml

--- Introduce_Me/app/src/main/java/com/example/introduce_me/SignInActivity.kt
+++ Introduce_Me/app/src/main/java/com/example/introduce_me/SignInActivity.kt
@@ -3,9 +3,9 @@ package com.example.introduce_me
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signin)
     }
 }
