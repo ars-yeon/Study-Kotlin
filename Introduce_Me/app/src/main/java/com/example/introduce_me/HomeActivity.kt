4047e49b0f6d9913836c0cb18f576a0ac3bf7759
ars-yeon <zosel7301@gmail.com>
2023-08-03 21:37:24 +0900

    [FEAT] : ⚙️ - SignInActivity에서 extra data로 전달받은 아이디 화면에 띄우기

--- Introduce_Me/app/src/main/java/com/example/introduce_me/HomeActivity.kt
+++ Introduce_Me/app/src/main/java/com/example/introduce_me/HomeActivity.kt
@@ -2,10 +2,18 @@ package com.example.introduce_me
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
+// 3. 자기소개 페이지
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        // SignInActivity에서 extra data로 전달받은 아이디를 화면에 표시
+        val strData = intent.getStringExtra("datafromSignInActivity")
+        val extra_id = findViewById<TextView>(R.id.extra_id)
+        val idDisplayFormat = getString(R.string.id_display_format, strData)
+        extra_id.setText(idDisplayFormat)
     }
 }
