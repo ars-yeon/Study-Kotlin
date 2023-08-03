883e463f12c026f45f901688e70e90015889d114
ars-yeon <zosel7301@gmail.com>
2023-08-03 16:26:03 +0900

    [FEAT] : ⚙️ - activity_sign_up.xml UI 구성 - SignInActivity, SignUpActivity 연결

--- Introduce_Me/app/src/main/java/com/example/introduce_me/SignInActivity.kt
+++ Introduce_Me/app/src/main/java/com/example/introduce_me/SignInActivity.kt
@@ -1,11 +1,26 @@
 package com.example.introduce_me
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        /*val btn_sign_in = findViewById<Button>(R.id.btn_sign_in)
+        btn_sign_in.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }*/
+
+        // 가입하기 페이지로 이동
+        val btn_sign_up = findViewById<Button>(R.id.btn_sign_up)
+        btn_sign_up.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- Introduce_Me/app/src/main/java/com/example/introduce_me/SignUpActivity.kt
+++ Introduce_Me/app/src/main/java/com/example/introduce_me/SignUpActivity.kt
@@ -2,10 +2,17 @@ package com.example.introduce_me
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        // 가입하기 버튼이 눌리면 SignInActivity로 이동
+        val btn_log_in = findViewById<Button>(R.id.btn_sign_up)
+        btn_log_in.setOnClickListener {
+            finish()
+        }
     }
 }
