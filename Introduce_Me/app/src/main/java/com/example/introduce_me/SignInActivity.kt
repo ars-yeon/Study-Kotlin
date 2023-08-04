bdc47211231eac8ced1d5278506b2ec86da42b2c
ars-yeon <zosel7301@gmail.com>
2023-08-04 22:41:55 +0900

    [DOCS] : 📚️ - 주석 추가 및 수정

--- Introduce_Me/app/src/main/java/com/example/introduce_me/SignInActivity.kt
+++ Introduce_Me/app/src/main/java/com/example/introduce_me/SignInActivity.kt
@@ -11,13 +11,15 @@ import androidx.activity.result.contract.ActivityResultContracts
 
 // 1. 로그인 페이지
 class SignInActivity : AppCompatActivity() {
+    // SignUpActivity를 위한 ActivityResultLauncher를 선언
     private lateinit var signInLauncher: ActivityResultLauncher<Intent>  // SignUpActivity를 위한 ActivityResultLauncher
+
+    // UI 요소 연결할 변수 선언
     private lateinit var editId: EditText
     private lateinit var editPw: EditText
     private lateinit var btnSignIn: Button
     private lateinit var btnSignUp: Button
 
-    // Activity가 생성되면 호출되는 메서드
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
@@ -28,11 +30,12 @@ class SignInActivity : AppCompatActivity() {
         btnSignIn = findViewById(R.id.btn_sign_in)
         btnSignUp = findViewById(R.id.btn_sign_up)
 
-        // SignUpActivity 실행하고 그 결과를 처리하기 위해 ActivityResultLauncher 초기화
+        // registerForActivityResult를 사용하여 signUpLauncher를 초기화
+        // signUpLauncher는 SignUpActivity를 호출하고 그 결과를 처리할 때 사용
         signInLauncher =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                // SignUpActivity가 성공적으로 완료되었을 때
-                if (result.resultCode == RESULT_OK) {
+                // SignUpActivity가 성공적으로 완료되었을 때 처리하는 람다식
+                if (result.resultCode == RESULT_OK) {  // 결과가 OK인 경우에만 처리
                     val data: Intent? = result.data
 
                     // SignUpActivity에서 입력한 ID/PW를 가져옴
@@ -40,7 +43,7 @@ class SignInActivity : AppCompatActivity() {
                         val id = data.getStringExtra("inputId")
                         val pw = data.getStringExtra("inputPw")
 
-                        // SignInActivity ID 입력 필드에 SignUPActivity에서 가져온 ID/PW를 자동으로 채워넣음
+                        // SignInActivity ID/PW 입력 필드에 SignUPActivity에서 가져온 ID/PW를 자동으로 채워넣음
                         if (!id.isNullOrEmpty() && !pw.isNullOrEmpty()) {
                             editId.setText(id)
                             editPw.setText(pw)
@@ -49,24 +52,25 @@ class SignInActivity : AppCompatActivity() {
                 }
             }
 
-        // 로그인 버튼
+        // 로그인 버튼이 눌렸을 때의 동작 정의
         btnSignIn.setOnClickListener {
             val id = editId.text.toString() // 입력한 ID를 가져옴
             val pw = editPw.text.toString() // 입력한 PW를 가져옴
 
-            // ID/PW가 둘 다 입력되었다면 HomeActivity로 입력한 ID 전달, HomeActivity 실행
+            // ID/PW가 모두 채워져 있다면 HomeActivity로 입력한 ID 전달, HomeActivity 실행
             if (id.isNotEmpty() && pw.isNotEmpty()) {
                 val intent = Intent(this, HomeActivity::class.java)
                 intent.putExtra("inputId", id)
                 startActivity(intent)
 
                 Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
-            } else {  // ID/PW가 둘 중 하나라도 입력을 안했으면
+            } else {  // ID나 PW 중 하나라도 입력되지 않았을 경우
+                // 토스트 메시지를 통해 아이디/비밀번호를 확인하라는 안내를 띄움
                 Toast.makeText(this, "아이디/비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show()
             }
         }
 
-        // 가입하기 버튼
+        // 가입하기 버튼이 눌렸을 때의 동작 정의
         btnSignUp.setOnClickListener {
             // SignUpActivity를 호출하고 결과를 처리할 때 signInLauncher를 사용
             val intent = Intent(this, SignUpActivity::class.java)
