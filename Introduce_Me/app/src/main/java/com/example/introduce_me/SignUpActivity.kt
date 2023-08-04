ddb267391b3d740d9101e55db2a906ee7d1ca53f
ars-yeon <zosel7301@gmail.com>
2023-08-04 22:41:36 +0900

    [FEAT] : ⚙️ - intent, putExtra: 회원 가입페이지에서 입력한 아이디/비밀번호가 로그인 화면으로 돌아올 때 자동 입력되는 기능 구현 - 변수 선언 위치 변경, val → private lateinit var - 변수명에 _ 들어가는 경우 수정(ex. btn_sign_in → btnSignIn) - 주석 추가 및 수정

--- Introduce_Me/app/src/main/java/com/example/introduce_me/SignUpActivity.kt
+++ Introduce_Me/app/src/main/java/com/example/introduce_me/SignUpActivity.kt
@@ -1,31 +1,80 @@
 package com.example.introduce_me
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 
 // 2. 회원가입 페이지
 class SignUpActivity : AppCompatActivity() {
+    // ActivityResultLauncher를 선언하여 startActivityForResult를 대체할 준비
+    private lateinit var signUpLauncher: ActivityResultLauncher<Intent>
+
+    // UI 요소 연결할 변수 선언
+    private lateinit var editName: EditText
+    private lateinit var editId: EditText
+    private lateinit var editPw: EditText
+    private lateinit var btnSignUp: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        val edit_name = findViewById<EditText>(R.id.edit_name)
-        val edit_id = findViewById<EditText>(R.id.edit_id)
-        val edit_pw = findViewById<EditText>(R.id.edit_pw)
-        val btn_log_in = findViewById<Button>(R.id.btn_sign_up)
+        // UI 요소 연결
+        editName = findViewById(R.id.edit_name)
+        editId = findViewById(R.id.edit_id)
+        editPw = findViewById(R.id.edit_pw)
+        btnSignUp = findViewById(R.id.btn_sign_up)
 
-        // 가입하기 버튼이 눌리면 SignInActivity로 이동
-        btn_log_in.setOnClickListener {
-            val name = edit_name.text.toString()
-            val id = edit_id.text.toString()
-            val pw = edit_pw.text.toString()
+        // registerForActivityResult 호출하여 signUpLauncher 초기화
+        signUpLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+                // startActivityForResult 결과를 처리하는 람다식
+                if (result.resultCode == RESULT_OK) {
+                    val data = result.data
+                    if (data != null) {
+                        // 결과로 전달된 데이터에서 ID/PW를 가져옴
+                        val id = data.getStringExtra("inputId")
+                        val pw = data.getStringExtra("inputPw")
 
+                        // ID/PW가 모두 비어있지 않은 경우
+                        if (!id.isNullOrEmpty() && !pw.isNullOrEmpty()) {
+                            // Intent ID/PW를 담음
+                            val intent = Intent()
+                            intent.putExtra("inputId", id)
+                            intent.putExtra("inputPw", pw)
+
+                            // 결과 설정, 현재 액티비티 종료
+                            setResult(RESULT_OK, intent)
+                            finish()
+                        }
+                    }
+                }
+            }
+
+        // 가입하기 버튼이 눌렸을 때 동작 정의
+        btnSignUp.setOnClickListener {
+            val name = editName.text.toString()
+            val id = editId.text.toString()
+            val pw = editPw.text.toString()
+
+            // Name/ID/PW가 모두 채워져 있을 경우
             if (name.isNotEmpty() && id.isNotEmpty() && pw.isNotEmpty()) {  // 이름/아이디/비번 셋 다 채워져 있다면
-                finish()  // SignInActivity로 돌아가기
-            } else {  // // 이름/아이디/비번 중 하나라도 비어있다면
+                // 결과로 전달할 데이터를 Intent에 담음
+                val intent = Intent()
+                intent.putExtra("inputId", id)
+                intent.putExtra("inputPw", pw)
+
+                // 결과를 설정, 현재 액티비티 종료
+                setResult(RESULT_OK, intent)
+                finish()  // SignInActivity 돌아가기
+            } else {  // Name/ID/PW 중 하나라도 비어있을 경우
+                // 토스트 메시지를 통해 입력되지 않은 정보가 있다는 메시지를 띄움
                 Toast.makeText(this, "입력되지 않은 정보가 있습니다.", Toast.LENGTH_SHORT).show()
             }
         }
