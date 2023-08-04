3cb82f504b7e2b5a62446f9ecd151c7a280c600a
ars-yeon <zosel7301@gmail.com>
2023-08-04 22:27:02 +0900

    [FEAT] : ⚙️ - registerForActivityResult: 회원 가입페이지에서 입력한 아이디/비밀번호가 로그인 화면으로 돌아올 때 자동 입력되는 기능 구현 - 변수 선언 위치 변경, val → private lateinit var - 변수명에 _ 들어가는 경우 수정(ex. btn_sign_in → btnSignIn)

--- Introduce_Me/app/src/main/java/com/example/introduce_me/SignInActivity.kt
+++ Introduce_Me/app/src/main/java/com/example/introduce_me/SignInActivity.kt
@@ -6,38 +6,71 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 
 // 1. 로그인 페이지
 class SignInActivity : AppCompatActivity() {
+    private lateinit var signInLauncher: ActivityResultLauncher<Intent>  // SignUpActivity를 위한 ActivityResultLauncher
+    private lateinit var editId: EditText
+    private lateinit var editPw: EditText
+    private lateinit var btnSignIn: Button
+    private lateinit var btnSignUp: Button
+
+    // Activity가 생성되면 호출되는 메서드
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
-        val edit_id = findViewById<EditText>(R.id.edit_id)
-        val edit_pw = findViewById<EditText>(R.id.edit_pw)
-        val btn_sign_in = findViewById<Button>(R.id.btn_sign_in)
+        // UI 요소 연결
+        editId = findViewById(R.id.edit_id)
+        editPw = findViewById(R.id.edit_pw)
+        btnSignIn = findViewById(R.id.btn_sign_in)
+        btnSignUp = findViewById(R.id.btn_sign_up)
 
-        // 로그인하기 버튼 눌렀을때
-        btn_sign_in.setOnClickListener {
-            val id = edit_id.text.toString()
-            val pw = edit_pw.text.toString()
+        // SignUpActivity 실행하고 그 결과를 처리하기 위해 ActivityResultLauncher 초기화
+        signInLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                // SignUpActivity가 성공적으로 완료되었을 때
+                if (result.resultCode == RESULT_OK) {
+                    val data: Intent? = result.data
 
-            if (id.isNotEmpty() && pw.isNotEmpty()) {  // 아이디/비번 둘 다 채워져 있다면
-                val intent = Intent(this, HomeActivity::class.java)  // 자기소개 페이지로 이동하는 변수 Intent
-                intent.putExtra("datafromSignInActivity", id)
-                startActivity(intent)  // 자기소개 페이지로 이동
+                    // SignUpActivity에서 입력한 ID/PW를 가져옴
+                    if (data != null) {
+                        val id = data.getStringExtra("inputId")
+                        val pw = data.getStringExtra("inputPw")
 
-                Toast.makeText(this, "로그인 성공",  Toast.LENGTH_SHORT).show()  // 토스트 메시지 띄우기
-            } else { // 아이디/비번 중 하나라도 비어있다면
-                Toast.makeText(this, "아이디/비밀번호를 확인해주세요.",  Toast.LENGTH_SHORT).show()
+                        // SignInActivity ID 입력 필드에 SignUPActivity에서 가져온 ID/PW를 자동으로 채워넣음
+                        if (!id.isNullOrEmpty() && !pw.isNullOrEmpty()) {
+                            editId.setText(id)
+                            editPw.setText(pw)
+                        }
+                    }
+                }
+            }
+
+        // 로그인 버튼
+        btnSignIn.setOnClickListener {
+            val id = editId.text.toString() // 입력한 ID를 가져옴
+            val pw = editPw.text.toString() // 입력한 PW를 가져옴
+
+            // ID/PW가 둘 다 입력되었다면 HomeActivity로 입력한 ID 전달, HomeActivity 실행
+            if (id.isNotEmpty() && pw.isNotEmpty()) {
+                val intent = Intent(this, HomeActivity::class.java)
+                intent.putExtra("inputId", id)
+                startActivity(intent)
+
+                Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
+            } else {  // ID/PW가 둘 중 하나라도 입력을 안했으면
+                Toast.makeText(this, "아이디/비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show()
             }
         }
 
-        // 가입하기 페이지로 이동
-        val btn_sign_up = findViewById<Button>(R.id.btn_sign_up)
-        btn_sign_up.setOnClickListener {
+        // 가입하기 버튼
+        btnSignUp.setOnClickListener {
+            // SignUpActivity를 호출하고 결과를 처리할 때 signInLauncher를 사용
             val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
+            signInLauncher.launch(intent) // SignUpActivity를 실행하고 결과를 처리하는데 사용할 signInLauncher를 사용하여 호출
         }
     }
 }
