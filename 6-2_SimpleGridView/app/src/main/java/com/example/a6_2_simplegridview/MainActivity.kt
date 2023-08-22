8c69b0234ef0117d11962e3eaeb93c1643919f9b
ars-yeon <zosel7301@gmail.com>
2023-08-22 20:10:31 +0900

    [FEAT] : ⚙️ Created ArrayAdapter Object and Linked it to GridView

--- 6-2_SimpleGridView/app/src/main/java/com/example/a6_2_simplegridview/MainActivity.kt
+++ 6-2_SimpleGridView/app/src/main/java/com/example/a6_2_simplegridview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.a6_2_simplegridview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.a6_2_simplegridview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +14,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        // 데이터 원본 준비
+        val items = arrayOf<String?>("item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item5", "item6", "item7", "item8", "item5", "item6", "item7", "item8", "item5", "item6",  "item7", "item8")
+
+        //어댑터 준비 (배열 객체 이용, simple_list_item_1 리소스 사용
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
+
+        // 어댑터를 GridView 객체에 연결
+        binding.gridview.adapter = adapter
     }
 }
