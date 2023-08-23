e3689c9bf57b5605c1fb88fe56362e128d956bf6
ars-yeon <zosel7301@gmail.com>
2023-08-23 11:57:17 +0900

    [FEAT] : ⚙️ Handled item click event

--- 6-3_ImageGridView/app/src/main/java/com/example/a6_3_imagegridview/MainActivity.kt
+++ 6-3_ImageGridView/app/src/main/java/com/example/a6_3_imagegridview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.a6_3_imagegridview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.a6_3_imagegridview.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
@@ -10,10 +11,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
         
         // ImageAdapter 객체를 생성하고 GridView 객체에 연결
         binding.gridview.adapter = ImageAdapter()
+
+        // 항목 클릭 이벤트 처리
+        binding.gridview.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this@MainActivity,"" + (position + 1) + "번째 선택", Toast.LENGTH_SHORT).show()
+        }
     }
 }
