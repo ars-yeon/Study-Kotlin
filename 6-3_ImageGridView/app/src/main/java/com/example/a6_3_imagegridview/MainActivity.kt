de0a6deabb0ad1dc9a0aaaad38e00f76555b6468
ars-yeon <zosel7301@gmail.com>
2023-08-23 11:25:50 +0900

    [FEAT] : ⚙️ Created an adapter and connected it to the GridView

--- 6-3_ImageGridView/app/src/main/java/com/example/a6_3_imagegridview/MainActivity.kt
+++ 6-3_ImageGridView/app/src/main/java/com/example/a6_3_imagegridview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.a6_3_imagegridview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import com.example.a6_3_imagegridview.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        
+        // ImageAdapter 객체를 생성하고 GridView 객체에 연결
+        binding.gridview.adapter = ImageAdapter()
     }
 }
