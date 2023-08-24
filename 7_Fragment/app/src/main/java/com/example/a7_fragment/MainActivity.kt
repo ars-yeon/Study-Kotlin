c94e7a973ca32d85e81d50ed03305e7822ac790f
ars-yeon <zosel7301@gmail.com>
2023-08-24 15:10:11 +0900

    [FEAT] : ⚙️ Add Fragment to MainActivity

--- 7_Fragment/app/src/main/java/com/example/a7_fragment/MainActivity.kt
+++ 7_Fragment/app/src/main/java/com/example/a7_fragment/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.a7_fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import com.example.a7_fragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.apply {
+            fragment1Btn.setOnClickListener{
+                setFragment(FirstFragment())
+            }
+            fragment2Btn.setOnClickListener {
+                setFragment(SecondFragment())
+            }
+        }
+        setFragment(FirstFragment())
+    }
+
+    private fun setFragment(frag: Fragment) {
+        supportFragmentManager.commit {
+            replace(R.id.frameLayout, frag)
+            setReorderingAllowed(true)
+            addToBackStack("")
+        }
     }
 }
