b56ad1850590ed3af6af08e095f97e4cfb2129d3
ars-yeon <zosel7301@gmail.com>
2023-08-24 16:37:46 +0900

    [FEAT] : ⚙️ Implement Custom Dialog

--- 8_Dialog/app/src/main/java/com/example/a8_dialog/MainActivity.kt
+++ 8_Dialog/app/src/main/java/com/example/a8_dialog/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.a8_dialog
 import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import com.example.a8_dialog.databinding.ActivityMainBinding
 
@@ -37,6 +38,30 @@ class MainActivity : AppCompatActivity() {
             builder.setPositiveButton("Positive", listener)
             builder.setNegativeButton("Negative", listener)
             builder.setNeutralButton("Neutral", listener)
+
+            builder.show()
+        }
+
+        // 2. 커스텀 다이얼로그
+        binding.btn2Custom.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("커스텀 다이얼로그")
+            builder.setIcon(R.mipmap.ic_blue_heart)
+
+            val v1 = layoutInflater.inflate(R.layout.dialog, null)
+            builder.setView(v1)
+
+            // p0에 해당 AlertDialog가 들어온다. findViewById를 통해 view를 가져와서 사용
+            val listener = DialogInterface.OnClickListener { p0, p1 ->
+                val alert = p0 as AlertDialog
+                val edit1: EditText? = alert.findViewById(R.id.editText)
+                val edit2: EditText? = alert.findViewById(R.id.editText2)
+
+                binding.tvTitle.text = "이름: ${edit1?.text}"
+                binding.tvTitle.append(" / 나이: ${edit2?.text}")
+            }
+            builder.setPositiveButton("확인", listener)
+            builder.setNegativeButton("취소", null)
 
             builder.show()
         }
