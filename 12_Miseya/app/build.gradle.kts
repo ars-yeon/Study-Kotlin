f292cb70d45fd0dc96e76cce7d295c7590889088
ars-yeon <zosel7301@gmail.com>
2023-09-14 14:39:07 +0900

    [FEAT] : ⚙️(build.gradle): Project Configuration and Dependency Upgrade
    - Raised compile SDK version to 34.
    - Updated dependencies to the latest AndroidX libraries.
    - Added DUST_API_KEY from local.properties to buildConfigField.
    - Enabled View Binding and BuildConfig features.
    - Added Gson, Retrofit, OkHttp, and Powerspinner libraries to dependencies.

--- 12_Miseya/app/build.gradle.kts
+++ 12_Miseya/app/build.gradle.kts
@@ -1,20 +1,32 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
 
+val properties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localPropertiesFile.inputStream().use { input ->
+        properties.load(input)
+    }
+}
+
 android {
     namespace = "com.example.a12_miseya"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.a12_miseya"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        buildConfigField("String", "DUST_API_KEY", "${properties["DUST_API_KEY"]}")
+
     }
 
     buildTypes {
@@ -33,14 +45,23 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("com.github.skydoves:powerspinner:1.2.6")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
